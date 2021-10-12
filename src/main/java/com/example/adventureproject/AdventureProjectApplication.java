221f439273f96ff0b4ca115a1b9281700a3e4e1a
Jev22 <50078304+Jev22@users.noreply.github.com>
2021-10-12 10:27:53 +0200

    Initial commit

---
+++ src/main/java/com/example/adventureproject/AdventureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.adventureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureProjectApplication.class, args);
+    }
+
+}
