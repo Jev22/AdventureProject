81efdd836546f764cbaf84a30e9234c53405a2d5
NicklasSpendler <KN6s~?c$%+T>
2021-10-13 11:04:30 +0200

    Added database connection details

--- src/main/java/com/example/adventureproject/controller/ActivityController.java
+++ src/main/java/com/example/adventureproject/controller/ActivityController.java
@@ -1,4 +1,33 @@
 package com.example.adventureproject.controller;
 
+import com.example.adventureproject.model.Activities;
+import com.example.adventureproject.repository.ActivitiesRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(value = "*")
 public class ActivityController {
+
+    @Autowired
+    ActivitiesRepository activitiesRepository;
+
+    @PostMapping(value="/activity", consumes = "application/json")
+
+
+
+
+    @DeleteMapping("activity/{id}")
+    public ResponseEntity<Object> deleteActivity (@PathVariable int id) {
+        try {
+            activitiesRepository.deleteById(id);
+        } catch (Exception err) {
+            return new ResponseEntity<>("Kommune ikke fundet", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
