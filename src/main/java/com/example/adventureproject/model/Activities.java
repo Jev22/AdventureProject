627fec8a9e13bd7e4ac96c025189d08e74bbf7b7
Jev22 <50078304+Jev22@users.noreply.github.com>
2021-10-13 10:28:00 +0200

    Packages Start repo + model

--- src/main/java/com/example/adventureproject/model/Activities.java
+++ src/main/java/com/example/adventureproject/model/Activities.java
@@ -1,4 +1,18 @@
 package com.example.adventureproject.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Activities {
+
+    @Id
+    private int activityID;
+    private String name;
+    private int price;
+    private int min_age;
+    private int min_height;
+    private int participants;
+
 }
--- src/main/java/com/example/adventureproject/repository/ActivitiesRepository.java
+++ src/main/java/com/example/adventureproject/repository/ActivitiesRepository.java
@@ -1,4 +1,22 @@
 package com.example.adventureproject.repository;
 
-public class ActivitiesRepository {
+import com.example.adventureproject.model.Activities;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ActivitiesRepository extends JpaRepository<Activities, Integer> {
+
+        List<Activities> findActivitiesByActivities(String Activities);
 }
+
+
+/*
+public interface KommuneRepository extends JpaRepository<Kommune, Integer> {
+
+    List<Kommune> findKommuneByRegionKode(String regionKode);
+
+    List<Kommune> findKommuneByKommune(String kommune);
+
+}
+*/
