91f6ca062121d0b014810d7fbb82ee5cc023b174
Jev22 <50078304+Jev22@users.noreply.github.com>
2021-10-13 10:43:26 +0200

    Packages Update model

--- src/main/java/com/example/adventureproject/model/Activities.java
+++ src/main/java/com/example/adventureproject/model/Activities.java
@@ -7,12 +7,66 @@ import javax.persistence.Id;
 @Entity
 public class Activities {
 
+
     @Id
     private int activityID;
+
     private String name;
     private int price;
     private int min_age;
     private int min_height;
     private int participants;
+
+    public Activities(){
+}
+
+    public int getActivityID() {
+        return activityID;
+    }
+
+    public void setActivityID(int activityID) {
+        this.activityID = activityID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMin_age() {
+        return min_age;
+    }
+
+    public void setMin_age(int min_age) {
+        this.min_age = min_age;
+    }
+
+    public int getMin_height() {
+        return min_height;
+    }
+
+    public void setMin_height(int min_height) {
+        this.min_height = min_height;
+    }
+
+    public int getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(int participants) {
+        this.participants = participants;
+    }
+
 
 }
