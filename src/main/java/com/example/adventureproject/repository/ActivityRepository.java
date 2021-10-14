94a123bc9766a9f26265e8ae4145d8e9cf2377be
Jev22 <50078304+Jev22@users.noreply.github.com>
2021-10-14 08:59:44 +0200

    Added 2 - Get allActivity() - Get all by id method

---
+++ src/main/java/com/example/adventureproject/repository/ActivityRepository.java
@@ -0,0 +1,11 @@
+package com.example.adventureproject.repository;
+
+import com.example.adventureproject.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ActivityRepository extends JpaRepository<Activity, Integer> {
+
+    List<Activity> findActivityByName(String activity);
+}
