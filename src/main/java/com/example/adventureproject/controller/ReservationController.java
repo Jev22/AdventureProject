30822fcf68c19bb559353095180f3efb57fa812b
NicklasSpendler <KN6s~?c$%+T>
2021-10-23 14:46:45 +0200

    getReservation methods

--- src/main/java/com/example/adventureproject/controller/ReservationController.java
+++ src/main/java/com/example/adventureproject/controller/ReservationController.java
@@ -5,10 +5,10 @@ import com.example.adventureproject.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -17,7 +17,21 @@ public class ReservationController {
     @Autowired
     ReservationRepository reservationRepository;
 
+    @GetMapping("/reservation")
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
 
+    @GetMapping("/reservation/{reservationID}")
+    public Reservation getReservation(@PathVariable int reservationID) {
+        Optional<Reservation> obj = reservationRepository.findById(reservationID);
+        if (obj.isPresent()) {
+            return obj.get();
+        }
+        Reservation reservation = new Reservation();
+        reservation.setName("Not Found");
+        return reservation;
+    }
 
     @PostMapping(value="/reservation", consumes = "application/json")
     public ResponseEntity<Reservation> newTimeSlot(@RequestBody Reservation reservation) {
@@ -25,7 +39,6 @@ public class ReservationController {
         reservationRepository.save(reservation);
         System.out.println("Done save reservation=" + reservation);
         return new ResponseEntity<Reservation>(reservation, HttpStatus.CREATED);
-
     }
 
 
